84d58332c14f9451f2b1a4a378139184a8046295
youbs <hubert.moutot@gmail.com>
2013-12-29 18:40:09 +0100

    fixed return response handling to use decrypted response

--- Plugin/ReturnHandler.php
+++ Plugin/ReturnHandler.php
@@ -3,10 +3,8 @@
 namespace Kptive\PaymentSipsBundle\Plugin;
 
 use JMS\Payment\CoreBundle\PluginController\PluginControllerInterface;
-use JMS\Payment\CoreBundle\Model\PaymentInterface;
 use JMS\Payment\CoreBundle\PluginController\Result;
 use Doctrine\ORM\EntityManager;
-use Kptive\PaymentSipsBundle\Client\Client;
 
 /**
  * @author Hubert Moutot <hubert.moutot@gmail.com>
@@ -14,19 +12,10 @@ use Kptive\PaymentSipsBundle\Client\Client;
 class ReturnHandler
 {
 
-    const SUCCESS = 1;
-
-    const FAILED = 2;
-
     /**
      * @var EntityManager
      */
     protected $entityManager;
-
-    /**
-     * @var Client
-     */
-    protected $client;
 
     /**
      * @var PluginControllerInterface
@@ -36,43 +25,41 @@ class ReturnHandler
     /**
      * Constructor
      *
-     * @param EntityManager $entityManager an EntityManager instance
-     * @param Client        $client        a Client instance
+     * @param EntityManager             $entityManager    an EntityManager instance
+     * @param PluginControllerInterface $pluginController a PluginControllerInterface instance
      */
-    public function __construct(EntityManager $entityManager, Client $client, PluginControllerInterface $pluginController)
+    public function __construct(EntityManager $entityManager, PluginControllerInterface $pluginController)
     {
         $this->entityManager = $entityManager;
-        $this->client = $client;
         $this->pluginController = $pluginController;
     }
 
     /**
-     * Handles the notification
+     * Processes the payment
      *
-     * @param string $data the encrypted data provided by the bank
+     * @param PaymentInstruction $instruction the PaymentInstruction to handle
+     * @param array              $data        the decrypted response data
      *
-     * @return int NotificationHandler::SUCCESS|NotificationHandler::FAILED
+     * @return Result
      */
     public function handle($instruction, $data)
     {
-        $response = $this->client->handleResponseData($data);
-
         if (null === $pendingTransaction = $instruction->getPendingTransaction()) {
             $payment = $this->pluginController->createPayment($instruction->getId(), $instruction->getAmount() - $instruction->getDepositedAmount());
         } else {
             $payment = $pendingTransaction->getPayment();
         }
 
-        foreach ($response as $key => $val) {
+        foreach ($data as $key => $val) {
             $instruction->getExtendedData()->set($key, $val);
         }
 
-        $result = $pluginController->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
+        $result = $this->pluginController->approveAndDeposit($payment->getId(), $payment->getTargetAmount());
 
         if (Result::STATUS_SUCCESS !== $result->getStatus()) {
-            throw new \RuntimeException('Transaction was not successful: '.$result->getReasonCode());
+            throw new \RuntimeException('Transaction was not successful: ' . $result->getReasonCode());
         }
 
-        return self::SUCCESS;
+        return $result;
     }
 }
